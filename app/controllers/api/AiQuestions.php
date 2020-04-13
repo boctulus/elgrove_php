7a54875667bd170d5a5e2caebd5fefbfa0632e8b
Pablo Bozzolo <boctulus@gmail.com>
2020-04-13 17:29:18 -0300

    AI questions

---
+++ app/controllers/api/AiQuestions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace simplerest\controllers\api;
+
+use simplerest\controllers\MyApiController; 
+
+class AiQuestions extends MyApiController
+{ 
+    //static protected $folder_field = 'workspace';
+    //static protected $owned = false;
+    //static protected $guest_access = true;
+
+    function __construct()
+    {       
+        $this->scope['guest']      = ['read'];
+        $this->scope['registered'] = ['read'];
+        $this->scope['copropietario'] = ['read'];
+        parent::__construct();
+    }
+
+        
+} // end class
---
+++ app/models/AiQuestionsModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace simplerest\models;
+
+use simplerest\core\Model;
+use simplerest\libs\Factory;
+
+class AiQuestionsModel extends Model
+ { 	
+	protected $schema = [
+		'id' => 'INT',
+		'question' => 'STR',
+		'answer' => 'STR', 
+		'keywords' => 'STR',
+		'enabled' => 'INT', 
+		'created_at' => 'STR',
+		'updated_at' => 'STR',
+		'deleted_at' => 'STR'
+	];
+
+	protected $nullable = [];
+
+	protected $rules = [
+		'enabled' => ['type' =>  'bool']	
+	];
+
+    function __construct($db = NULL){
+        parent::__construct($db);
+    }	
+	
+}
