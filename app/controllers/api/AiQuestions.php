cef89cc51e0664b52d1fc1cdeca4f8864e47ca41
Pablo Bozzolo <boctulus@gmail.com>
2020-04-13 18:21:35 -0300

    .

--- app/controllers/api/AiQuestions.php
+++ app/controllers/api/AiQuestions.php
@@ -8,7 +8,7 @@ class AiQuestions extends MyApiController
 { 
     //static protected $folder_field = 'workspace';
     //static protected $owned = false;
-    //static protected $guest_access = true;
+    static protected $guest_access = true;
 
     function __construct()
     {       
